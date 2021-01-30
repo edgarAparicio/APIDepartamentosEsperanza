324b8476ef08328c54b7e4db558008507126690f
Edgar Guillermo Aparicio Haro <edgar_chato@live.com.mx>
2021-01-30 07:14:34 -0600

    Se agrega el patron de diseño Repository al proyecto

--- APIDepartamentosEsperanza/Startup.cs
+++ APIDepartamentosEsperanza/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,29 +13,71 @@ namespace APIDepartamentosEsperanza
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to add services to the container.
+        /// </summary>
+        /// <param name="services"></param>
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+            services.AddSwaggerGen(setupAction => 
+            {
+                setupAction.SwaggerDoc
+                (
+                    "DepartamentOpenAPISpecification",
+                    new Microsoft.OpenApi.Models.OpenApiInfo()
+                    {
+                        Title = "Departments API",
+                        Version = "1"
+                    }
+                );
+            
+            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+         
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseHttpsRedirection();
 
             app.UseRouting();
 
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(setupAction =>
+            {
+                setupAction.SwaggerEndpoint(
+                    "/swagger/DepartamentOpenAPISpecification/swagger.json",
+                    "Departament API"
+                    );
+                //Permite que al ejecutar la aplicacion la pantalla a mostrar sea la UI de swagger
+                setupAction.RoutePrefix = "";
+            });
+
+                
+
         }
     }
 }
